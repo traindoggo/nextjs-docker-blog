2059fcfa04a9ee4d8c71ba12ba9ba66cf4d3b7c4
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-03 00:31:39 +0900

    add top page link to header logo

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export function Header() {
   return (
     <div
       className={`p-4 bg-neutral-950
     border-b-4 border-b-neutral-900`}
     >
-      <h1 className="text-2xl">Header</h1>
+      <Link href="/" className="text-2xl">
+        Bloggier
+      </Link>
     </div>
   );
 }
