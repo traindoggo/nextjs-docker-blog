56f9ece49227facf50f49bbf7cdb966a995c2683
traindoggo <traindoggo.competitor.tasty@gmail.com>
2024-07-10 22:28:54 +0900

    add lil flexible css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { prisma } from "@/lib/db";
-import { truncate } from "@/lib/string";
 import Link from "next/link";
 
 export default async function Home() {
@@ -9,13 +8,15 @@ export default async function Home() {
     <main className="flex flex-col mt-4">
       <h1 className="text-2xl text-center mb-10">Top Page</h1>
 
-      {/* TODO: flex 2 columns */}
-      <div className={`grid grid-cols-2 mx-4 gap-4`}>
+      <div
+        className={`flex flex-col md:grid md:grid-cols-2
+          xl:grid xl:grid-cols-3 mx-4 gap-4`}
+      >
         {blogs.map((blog) => (
           <Link
             key={blog.id}
             href={`/blogs/${blog.id}`}
-            className={` px-4 py-2 h-52 border rounded
+            className={` px-4 py-2 h-30 md:h-44 border rounded
             border-neutral-500 hover:border-neutral-200
             hover:bg-neutral-900
             duration-100`}
@@ -24,7 +25,12 @@ export default async function Home() {
 
             <hr className="my-2" />
 
-            <p>{truncate(blog.content, 350)}</p>
+            <p
+              className={`text-lg
+                overflow-hidden text-ellipsis line-clamp-4`}
+            >
+              {blog.content}
+            </p>
           </Link>
         ))}
       </div>
